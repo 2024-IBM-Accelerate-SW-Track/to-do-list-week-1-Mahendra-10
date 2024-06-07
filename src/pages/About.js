55e1e7829109efc41ff7772f00c1c00752daef1f
Mahendra Bikram Shahi <mahendra@mahendras-air.lan>
2024-06-07 18:03:08 -0400

    Completed Introduction

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import img1 from '../assets/image1.jpg';
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={img1}
+              alt="Profile Pic"
+            />
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Mahendra Shahi</div>
+            <div className="brief_description">
+             What's up yall? I am Mahendra Shahi. I am a rising 
+             junior studying Computer Science at Johns Hopkins University.
+             On campus, I'm involved in Product Management Club, Nepalese Student Associatoon 
+             and South Asian club. My favorite things to do in my free time are play soccer, swimming and hangout with 
+             my friends.Fun fact about me is I can spin a notebook on my fingers. 
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
+import AddTodo from "../component/AddTodo.js";
+import Todos from "../component/todos.js";
+import "./Home.css";
 
 class Home extends Component {
   // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
-      // create your empty list here call it todos.
+      todos: [], // Initialize the empty list here
     };
   }
-  // the addTodo function simply creates a new array that includes the user submitted todo item and then
+
+  // The addTodo function simply creates a new array that includes the user-submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
-    // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
+    // In React, keys or ids in a list help identify which items have changed, been added, or removed. Keys
     // should not share duplicate values.
-    // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
+    // To avoid having duplicate values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
-    // dealing with a larger data sensitive project.
+    // dealing with a larger data-sensitive project.
     todo.id = Math.random();
     // An array that contains the current array and the new todo item
     let new_list = [...this.state.todos, todo];
@@ -24,11 +28,13 @@ class Home extends Component {
       todos: new_list,
     });
   };
+
   render() {
     return (
       <div className="Home">
-        <p> Replace this</p>
-        <h1>Todo's </h1>
+        <p>Replace this</p>
+        <Todos todos={this.state.todos} /> {/* Pass the todos state here */}
+        <AddTodo addTodo={this.addTodo} /> {/* Pass the addTodo function correctly */}
       </div>
     );
   }
